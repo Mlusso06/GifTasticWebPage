83a09198c30b89d6fac1f54a88b2d212b4c7f471
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-01-24 23:16:02 -0600

    adding some basic structure to start off the app.js

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+// need to create an api call, url and key -- see previous activities in class
+// get the info with the .ajax call
+// have your Promise to listen for the response
+// console.log the response to see what and where the gif is, and look for q, limmit, and ratings
+// need to create buttons, have a form button that will click / listen and will create a New button
+// need to be able to pause and animate the gifs
