6180ba0b939eeb304499ed4f7d79d75112da4fd6
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-01-27 22:48:57 -0600

    adding my links for jQuery, links to my css, and reset, building more structure for HTML, and my app.js

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,24 @@
+//start the document - so it's ready for loading
+// have a starting array of example buttons the user can load with
+
+// create the fuction that will create buttons to dump into the array
+
+// need an onclick listener to start the function
+
 // need to create an api call, url and key -- see previous activities in class
 // get the info with the .ajax call
 // have your Promise to listen for the response
+
+
 // console.log the response to see what and where the gif is, and look for q, limmit, and ratings
+
+
+
 // need to create buttons, have a form button that will click / listen and will create a New button
+
+
+
 // need to be able to pause and animate the gifs
+
+
+// need to write / create new buttons to the page
